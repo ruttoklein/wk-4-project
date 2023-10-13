87bca997930b1ddda383daecc97e4158aa84bcf9
ruttklein <klein.rutto@student.moringaschool.com>
2023-10-13 11:40:12 +0300

    css and html update

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   const teamsContainer = document.getElementById("teams");
   const commentForm = document.getElementById("comment-form");
@@ -17,7 +16,8 @@ document.addEventListener("DOMContentLoaded", () => {
             <p>Abbreviation: ${team.abbreviation}</p>
             <p>Conference: ${team.conference}</p>
             <p>Division: ${team.division}</p>
-            <div class="comments"
+            <div class="comments">
+              <!-- Display comments for each team here -->
             </div>
           </div>
         `;
@@ -49,5 +49,66 @@ commentForm.addEventListener("submit", (e) => {
           commentInput.value = "";
         }
       });
-  });
+    });
 });
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const teamsContainer = document.getElementById("teams");
+  const commentForm = document.getElementById("comment-form");
+  const commentInput = document.getElementById("comment-input");
+
+  fetch("https://www.balldontlie.io/api/v1/teams")
+    .then(res => res.json())
+    .then(data => {
+      data.data.forEach(team => {
+        let infoDiv = document.createElement("div");
+        infoDiv.className = "infoDiv";
+        infoDiv.innerHTML = `
+          <h2 class="team-name">${team.full_name}</h2>
+          <div class="team-details" style="display: none;">
+            <p>City: ${team.city}</p>
+            <p>Abbreviation: ${team.abbreviation}</p>
+            <p>Conference: ${team.conference}</p>
+            <p>Division: ${team.division}</p>
+            <div class="comments">
+              <!-- Display comments for each team here -->
+            </div>
+          </div>
+        `;
+        teamsContainer.appendChild(infoDiv);        
+        const teamName = infoDiv.querySelector(".team-name");
+        const teamDetails = infoDiv.querySelector(".team-details");
+        teamName.addEventListener("click", () => {
+          if (teamDetails.style.display === "none") {
+            teamDetails.style.display = "block";
+          } else {
+            teamDetails.style.display = "none";
+          }
+        });
+      });
+commentForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const commentText = commentInput.value;
+        if (commentText) {
+          const teamsDetails = document.querySelectorAll(".team-details");
+          teamsDetails.forEach(teamDetails => {
+            if (teamDetails.style.display === "block") {
+              const commentsSection = teamDetails.querySelector(".comments");
+              const commentElement = document.createElement("div");
+              commentElement.className = "comment";
+              commentElement.textContent = commentText;
+              commentsSection.appendChild(commentElement);
+            }
+          });
+          commentInput.value = "";
+        }
+      });
+    });
+});
+
+
+
+
